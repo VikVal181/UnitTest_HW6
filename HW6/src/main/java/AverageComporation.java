1af4bc6c750b1506141b3b253bfa39d2954d6291
Viktoriia Valuykina <v.valuykina@air-burg.ru>
2023-10-24 23:07:08 +0300

    First commit

---
+++ HW6/src/main/java/AverageComporation.java
@@ -0,0 +1,30 @@
+
+import java.util.List;
+public class AverageComporation {
+    //Метод расчета среднего числа
+    public double calculateAverage(List<Integer> list) {
+        if (list == null || list.isEmpty()) {
+            throw new IllegalArgumentException("Список не может быть пустым или равным null");
+        }
+
+        int sum = 0;
+        for (int num : list) {
+            sum += num;
+        }
+        return (double) sum / list.size();
+    }
+
+//     Метод сравнения средних чисел, расчитанных методом calculateAverage
+    public String compareLists(List<Integer> list1, List<Integer> list2) {
+        double average1 = calculateAverage(list1);
+        double average2 = calculateAverage(list2);
+
+        if (average1 > average2) {
+            return "Первый список имеет большее среднее значение";
+        } else if (average2 > average1) {
+            return "Второй список имеет большее среднее значение";
+        } else {
+            return "Средние значения равны";
+        }
+    }
+}
---
+++ HW6/src/test/java/AverageComporationTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AverageComporationTest extends AverageComporation {
+
+    private AverageComporation comp;
+    private List<Integer> list1;
+    private List<Integer> list2;
+
+    @BeforeEach
+    public void preSet() {
+        comp = new AverageComporation();
+        list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        list2 = Arrays.asList(1, 2, 3);
+    }
+
+    @AfterEach
+    public void freeMem() {
+        comp = null;
+        list1 = null;
+        list2 = null;
+    }
+    @Test
+    void testCalculateAverageWithEmptyList() {
+        assertThrows(IllegalArgumentException.class, ()->comp.calculateAverage(null));
+    }
+
+    @Test
+    void testCalculateAveragetyList() {
+        double result = comp.calculateAverage(list1);
+        assertEquals(5.5, result);
+    }
+    @Test
+    void testCompareLists() {
+    }
+
+    @Test
+    public void testCompareListsFirstGrande() {
+        String result = comp.compareLists(list1, list2);
+        assertEquals("Первый список имеет большее среднее значение", result);
+    }
+
+    @Test
+    public void testCompareListsSecondGrande() {
+        String result = comp.compareLists(list2, list1);
+        assertEquals("Второй список имеет большее среднее значение", result);
+    }
+
+    @Test
+    public void testCompareListsEquals() {
+        String result = comp.compareLists(list1, list1);
+        assertEquals("Средние значения равны", result);
+    }
+}
